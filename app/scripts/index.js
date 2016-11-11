de860c6f8ef584d875bd4f6dd00d7aa3222a13d5
Matthew Harrington <mattharrington576@gmail.com>
2016-11-11 10:31:54 -0500

    Created models and collections

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+var $ = window.jQuery = window.$ = require('jquery');
+var Backbone = require('backbone');
+
+require('./router');
+
+// DOM Ready
+$(function(){
+  Backbone.history.start();
+});
---
+++ app/scripts/models/models.js
@@ -0,0 +1,84 @@
+var Backbone = require('backbone');
+
+var ParseModel = Backbone.Model.extend({
+  idAttribute: 'objectId',
+  save: function(key, val, options){
+    delete this.attributes.createdAt;
+    delete this.attributes.updatedAt;
+    return Backbone.Model.prototype.save.apply(this, arguments);
+  }
+});
+
+var ParseCollection = Backbone.Collection.extend({
+  whereClause: {field: '', className: '', objectId: ''},
+  parseWhere: function(field, className, objectId){
+    this.whereClause = {
+      field: field,
+      className: className,
+      objectId: objectId,
+      '__type': 'Pointer'
+    };
+    return this;
+  },
+
+  url: function(){
+    var url = this.baseUrl;
+    if(this.whereClause.field){
+      var field = this.whereClause.field;
+      delete this.whereClause.field;
+      url += '?where={"' + field + '":' + JSON.stringify(this.whereClause) + '}';
+    }
+    return url;
+  },
+
+  parse: function(data){
+    return data.results;
+  }
+});
+
+var Ingredient = ParseModel.extend({
+  defaults: {
+    name: '',
+    amount: 0,
+    units: ''
+  }
+});
+
+var IngredientCollection = ParseCollection.extend({
+  model: IngredientCollection,
+  baseUrl: 'https://shadow-of-the-colossus-server.herokuapp.com/classes/Ingredients'
+});
+
+var Recipe = ParseModel.extend({
+  defaults: {
+    name: '',
+    chefId: '',
+    servings: 0,
+    servingType: '',
+    ingredients: new IngredientCollection()
+  },
+
+  urlRoot: 'https://shadow-of-the-colossus-server.herokuapp.com/classes/Recipes',
+
+  save: function(key, val, options){
+    this.set('ingredients', this.get('ingredients').toJSON());
+    return ParseModel.prototype.save.apply(this, arguments);
+  },
+
+  parse: function(data){
+    data.ingredients = new IngredientCollection(data.ingredients);
+    return data;
+  }
+});
+
+var RecipeCollection({
+  model: Recipe,
+  url: 'https://shadow-of-the-colossus-server/herokuapp.com/classes/Recipes'
+});
+
+module.exports = {
+  Ingredient: Ingredient,
+  IngredientCollection: IngredientCollection,
+  Recipe: Recipe,
+  RecipeCollection: RecipeCollection
+};
